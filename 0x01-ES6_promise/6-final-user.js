2c5a3c311ba92ae961c314f0084c43e18c6fdcc4
Bolexzy <boluoyewumi79@gmail.com>
2023-06-29 17:36:38 +0100

    Add: Handle multiple promises

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,18 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
+
+  return Promise.allSettled(promises)
+    .then((results) => {
+      const finalResult = [];
+      results.forEach((result) => {
+        finalResult.push({
+          status: result.status,
+          value: result.value === 'undefined' ? `${result.reason}` : result.value,
+        });
+      });
+      return finalResult;
+    });
+}
